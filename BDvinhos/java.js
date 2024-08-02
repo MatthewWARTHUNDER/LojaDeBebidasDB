1951f9d775e5aa10d0fc9db20ead1464216046a3
MatthewTCC <165826800+MatthewWARTHUNDER@users.noreply.github.com>
2024-08-02 10:47:09 -0300

    Add files via upload

---
+++ BDvinhos/java.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.carousel-images img');
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    if (slideIndex < 0) { slideIndex = slides.length - 1; }
+
+    const offset = -slideIndex * 100; 
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
+
+
+showSlides();
+setInterval(() => moveSlide(1), 4000); 
